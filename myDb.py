c183c0383a03215a5fb683f41dad78fd58d4ed6d
Ihor N <js.rcnit@gmail.com>
2020-03-30 13:48:06 +0300

    test command class added

--- myDb.py
+++ myDb.py
@@ -3,9 +3,22 @@ import sys
 sys.path.append('./classes/import')
 
 from importData import ImportData
+from command import Command
 
 i = ImportData('./data/input/testDb.txt')
 
 i.importFromTxt()
 
-print(i.getDbs())
+#print(i.getDbs())
+
+cmd = Command(i.getDbs())
+
+req = {
+    "command" : "find",
+    "table" : "sales",
+    "condition" : {
+        "=" : ['city', 'London']  
+    }
+}
+
+cmd.doCommand(req)
